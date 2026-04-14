31b38a2018d43be64ebe03ae75d04aaf983a69aa
junaid1506 <junnu2757@gmail.com>
2026-04-14 12:20:11 +0530

    Q2. Sum and print with message

--- Index.js
+++ Index.js
@@ -1,3 +1,13 @@
-let a = 12;
-let b = 13;
-console.log(a + b);
+// Sum of two inteergers
+// let a = "12";
+// let b = 13;
+// console.log(a + b); //concatenation
+// console.log(a - b); //subtraction
+
+// Sum and print with message
+
+let a = 10;
+let b = 20;
+console.log("The sum of a and b is " + a + b); // The sum of a and b is 1020
+console.log("The sum of a and b is " + (a + b)); // The sum of a and b is 30
+console.log(a + b + " is a sum of a and b"); // 30 is a sum of a and b
