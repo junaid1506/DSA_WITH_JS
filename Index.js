f0954c37cc93216b05b804e52e0a68b13716f561
junaid1506 <junnu2757@gmail.com>
2026-04-16 13:04:41 +0530

    Practice questions

--- Index.js
+++ Index.js
@@ -13,5 +13,33 @@
 // console.log(a + b + " is a sum of a and b"); // 30 is a sum of a and b
 
 // Q3
-let age = Number(prompt("Enter your age"));
-console.log(age);
+// let age = Number(prompt("Enter your age"));
+// console.log(age);
+
+// Q4 Swapping of two numbers
+// method one
+// let a = 10;
+// let b = 20;
+// let c;
+// c = a;
+// a = b;
+// b = c;
+
+// console.log(a);
+// console.log(b);
+
+// method two
+// let a = 10;
+// let b = 20;
+
+// a = a + b; //30
+// b = a - b; // 10
+// a = a - b; // 20
+
+// console.log(a);
+// console.log(b);
+
+// Q5 Opreator
+
+let a = 9430;
+console.log(a % 10);
--- test.js
+++ test.js
@@ -1,12 +1,65 @@
-function convertIntoNumber(str) {
-  let intialValue = 0;
-  for (let i = 0; i < str.length; i++) {
-    console.log(str[i] * 10 + str[i]);
+// Q1
+
+// function factorial(n) {
+//   let sum = 1;
+//   for (let i = 1; i <= n; i++) {
+//     sum = sum * i;
+//   }
+//   return sum;
+// }
+
+// console.log(factorial(5));
+
+// Q2
+// function reverseString(str) {
+//   let element = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     element = element + str[i];
+//   }
+//   return element;
+// }
+
+// let abc = "abc";
+// let newOne = "";
+
+// newOne = abc[2];
+// newOne = newOne + abc[1];
+// newOne = newOne + abc[0];
+// console.log(newOne);
+
+// console.log(reverseString("abc"));
+
+//Q3
+// function findMax(arr) {
+//   let max = arr[0];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+
+// console.log(findMax([3, 17, 12, 9, 5]));
+
+//Q4
+
+function isPalindrome(str) {
+  let reverse = "";
+  let result = false;
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse = reverse + str[i];
   }
-  return intialValue;
+
+  if (reverse === str) {
+    result = true;
+  }
+
+  return result;
 }
 
-console.log(convertIntoNumber("123"));
-// let a = "123";
-// let b = convertIntoNumber(a);
-// console.log(b);
+
+
+console.log(isPalindrome('mama'))
