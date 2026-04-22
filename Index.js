026c481fd2b1e91609ddda66696aaabd36484440
junaid1506 <junnu2757@gmail.com>
2026-04-22 16:01:37 +0530

    Update

--- Index.js
+++ Index.js
@@ -82,5 +82,10 @@
 // console.log(areaOfReactangle);
 // console.log(paremeterOfReactangle);
 
+let age = 17;
 
-
+if (age >= 18) {
+  console.log("You are eligible to vote");
+} else {
+  console.log("You are not eligible to vote");
+}
