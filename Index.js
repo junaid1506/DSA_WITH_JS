8d2596fbc26da2d991b8077a915efdfc1b2c3db2
junaid1506 <junnu2757@gmail.com>
2026-05-02 10:56:22 +0530

    Update

--- Index.js
+++ Index.js
@@ -150,25 +150,39 @@
 //   }
 // }
 
+// let pr = prompt("Enter the number");
+// if (pr === null) {
+//   console.log("Cancelled");
+// } else {
+//   let num = Number(pr);
+//   if (isNaN(num)) {
+//     console.log("plaes enter the valid number");
+//   } else {
+//     if (num > 0) {
+//       let res = [];
+//       for (let i = 1; i <= num; i++) {
+//         if (num % i === 0) {
+//           res.push(i);
+//         }
+//       }
+//       console.log(res);
+//     } else {
+//       console.log("Number should be natural number ");
+//     }
+//   }
+// }
 
-let pr = prompt("Enter the number");
-if (pr === null) {
-  console.log("Cancelled");
-} else {
-  let num = Number(pr);
-  if (isNaN(num)) {
-    console.log("plaes enter the valid number");
-  } else {
-    if (num > 0) {
-      let res = [];
-      for (let i = 1; i <= num; i++) {
-        if (num % i === 0) {
-          res.push(i);
-        }
-      }
-      console.log(res);
-    } else {
-      console.log("Number should be natural number ");
-    }
-  }
+// While
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+
+let num = prompt("Kuch bhi de do (exit for close)");
+
+while (num !== "exit") {
+  num = prompt("Kuch bhi de do (exit for close)");
 }
