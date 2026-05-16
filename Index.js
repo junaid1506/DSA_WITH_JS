9bd59a5f52629bed1ed22149358313586154c5bc
junaid1506 <junnu2757@gmail.com>
2026-05-16 14:47:36 +0530

    Update

--- Index.js
+++ Index.js
@@ -321,4 +321,11 @@
 //   }
 // }
 
+// console.log("Hello ");
+// console.log("World");
 
+// process.stdout.write("Hello ");
+// process.stdout.write("World");
+
+let a = prompt("Enter the first number");
+console.log(a);
